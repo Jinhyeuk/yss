4324e8fc3494df8af034c1b317760889e89ccdc4
Yoon-Ki Hong <mymy49@nate.com>
2025-08-22 15:57:47 +0900

    누락된 라이선스 추가

--- src/scheduler/yss_Thread.cpp
+++ src/scheduler/yss_Thread.cpp
@@ -1,4 +1,11 @@
-#include <yss/Thread.h>
+/*
+ * Copyright (c) 2025 Yoon-Ki Hong
+ *
+ * This file is subject to the terms and conditions of the MIT License.
+ * See the file "LICENSE" in the main directory of this archive for more details.
+ */
+ 
+ #include <yss/Thread.h>
 
 static void thread_thread(void *var)
 {
