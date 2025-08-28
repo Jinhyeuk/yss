ba212459f4ea2c6338a3bfd95820eb45012cd00b
Yoon-Ki Hong <mymy49@nate.com>
2025-08-28 11:30:12 +0900

    mod/spi_tft_lcd/P028B052_V3_IPS class 업데이트
    - 구매 링크 수정 및 일부 문구 수정

--- inc/mod/spi_tft_lcd/P028B052_V3_IPS.h
+++ inc/mod/spi_tft_lcd/P028B052_V3_IPS.h
@@ -1,19 +1,17 @@
 /*
- * Copyright (c) 2015 Yoon-Ki Hong
+ * Copyright (c) 2024 Yoon-Ki Hong
  *
  * This file is subject to the terms and conditions of the MIT License.
  * See the file "LICENSE" in the main directory of this archive for more details.
  */
 
-#ifndef YSS_MOD_SPI_TFT_LCD_TOUCH_LCD_SHIELD_FOR_ARDUINO_2_8_INCH__H_
-#define YSS_MOD_SPI_TFT_LCD_TOUCH_LCD_SHIELD_FOR_ARDUINO_2_8_INCH__H_
+#ifndef YSS_MOD_SPI_TFT_LCD_P028B052_V3_IPS__H_
+#define YSS_MOD_SPI_TFT_LCD_P028B052_V3_IPS__H_
 
 #include <mod/tft_lcd_driver/ST7789V_spi_with_Brush_RGB565.h>
 
-// 구매처 : https://www.eleparts.co.kr/goods/view?no=3216739
-// 구매처 : https://www.waveshare.com/2.8inch-TFT-Touch-Shield.htm
+// 구매처 : https://www.alibaba.com/product-detail/Polcd-2-8-inch-Lcd-Display_1601210824541.html?spm=a2700.galleryofferlist.normal_offer.d_title.301213a0U102Ki&priceId=b473ad8892c0448ca8aa6dbb79fcdef5
 
-// 현재 이 장치는 정상 동작이 확인되지 않았습니다.
 class P028B052_V3_IPS : public ST7789V_spi_with_Brush_RGB565
 {
   public:
